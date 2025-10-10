0e74df6b223eb7ab3801eba2d568f2b1a79a3d05
YATHARTH SHARMA <Yatharth.sharma1117@outlook.com>
2025-10-10 19:24:05 +0530

    First Commit

---
+++ ListenerUI/ListenerUI/MainForm.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace ListenerUI
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPageWirelessComm = new System.Windows.Forms.TabPage();
+            this.tabControl1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPageWirelessComm);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(1355, 737);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPageWirelessComm
+            // 
+            this.tabPageWirelessComm.BackColor = System.Drawing.Color.Transparent;
+            this.tabPageWirelessComm.Location = new System.Drawing.Point(4, 25);
+            this.tabPageWirelessComm.Name = "tabPageWirelessComm";
+            this.tabPageWirelessComm.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPageWirelessComm.Size = new System.Drawing.Size(1347, 708);
+            this.tabPageWirelessComm.TabIndex = 0;
+            this.tabPageWirelessComm.Text = "Push Notification";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1355, 737);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "MainForm";
+            this.Text = "MainForm";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.tabControl1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPageWirelessComm;
+    }
+}
---
+++ ListenerUI/ListenerUI/MainForm.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ListenerUI
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+            AddListenerFormToTabPage();
+        }
+        private void AddListenerFormToTabPage()
+        {
+            if (tabPageWirelessComm != null)
+            {
+                ListenerForm listenerForm = new ListenerForm();
+                listenerForm.TopLevel = false;
+                listenerForm.FormBorderStyle = FormBorderStyle.None;
+                listenerForm.Dock = DockStyle.Fill;
+
+                tabPageWirelessComm.Controls.Clear();
+                tabPageWirelessComm.Controls.Add(listenerForm);
+                listenerForm.Show();
+            }
+            else
+            {
+                MessageBox.Show("tabPageLCDConfig not found in tabControl2.");
+            }
+        }
+    }
+}
